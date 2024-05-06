d943a16af1af687970ddfd2a916891939d915a70
samarali <engsamar105@gmail.com>
2024-05-06 04:25:00 +0300

    upload image

--- utils/imagekit.js
+++ utils/imagekit.js
@@ -3,8 +3,7 @@ const imageKit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-      authenticationEndpoint: `http://localhost:${process.env.PORT}/auth`
-    
+    authenticationEndpoint: `http://localhost:${process.env.PORT || 8000}/auth`    
     
     
     
