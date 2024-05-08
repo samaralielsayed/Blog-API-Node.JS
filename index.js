d66365890caccae0859cb0150756cf878602dcb8
samarali <engsamar105@gmail.com>
2024-05-08 23:34:09 +0300

    Edit upload img

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require("./db/dbConnection");
 //routes all methods from routing file
 const userRouter=require('./routes/users.route');
 const postRouter=require('./routes/posts.route');
-const { imageKit } = require("./utils/imagekit");
+// const { imageKit } = require("./utils/imagekit");
 
 
 
@@ -32,18 +32,18 @@ exp.use(express.json());
 
 // Route to authenticate with ImageKit
 // ImageKit authentication rendering
-exp.get('/auth', function (req, res) {
-  try {
-      const result = imageKit.getAuthenticationParameters();
-      res.json(result);
-  } catch (error) {
-      console.error("Error generating authentication parameters:", error.message);
-      res.status(500).json({
-          status: "failed",
-          message: "Authentication failed. Please check your ImageKit configuration."
-      });
-  }
-});
+// exp.get('/auth', function (req, res) {
+//   try {
+//       const result = imageKit.getAuthenticationParameters();
+//       res.json(result);
+//   } catch (error) {
+//       console.error("Error generating authentication parameters:", error.message);
+//       res.status(500).json({
+//           status: "failed",
+//           message: "Authentication failed. Please check your ImageKit configuration."
+//       });
+//   }
+// });
 
 exp.use("/api/users",userRouter);
 exp.use("/api/posts",postRouter);
--- utils/imagekit.js
+++ utils/imagekit.js
@@ -3,7 +3,7 @@ const imageKit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-    authenticationEndpoint: `http://localhost:${process.env.PORT || 8000}/auth`    
+    // authenticationEndpoint: `http://localhost:${process.env.PORT || 8000}/auth`    
     
     
     
